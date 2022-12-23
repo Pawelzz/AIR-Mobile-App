b563044fd0ad1e487df9cd2a0a59e89b46d64c0c
pskoczylas-air <65227471+pskoczylas-air@users.noreply.github.com>
2022-12-23 20:45:40 +0100

    Plots: Added plots in version 1.0

--- app/src/main/java/com/example/airmobileapp/PlotActivity.kt
+++ app/src/main/java/com/example/airmobileapp/PlotActivity.kt
@@ -3,11 +3,12 @@ package com.example.airmobileapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.graphics.Color
 import android.text.InputFilter
 import android.text.Spanned
 import android.util.Log
+import android.view.View
+import android.widget.*
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.LegendRenderer
 import com.jjoe64.graphview.series.DataPoint
@@ -26,6 +27,9 @@ class PlotActivity : AppCompatActivity() {
     private var counter: Int = 0
     private lateinit var timer: Timer
     private var started: Boolean = false
+    private val values = arrayOf("Pressure", "Humidity", "Temperature")
+    var val1: String = ""
+    var val2: String = ""
 
     // ***********************************************
 
@@ -79,6 +83,58 @@ class PlotActivity : AppCompatActivity() {
                 timer.scheduleAtFixedRate(timerTask, 0, period.toLong())
             }
             started = true
+        }
+
+        // access the first spinner
+        val spinner1 = findViewById<Spinner>(R.id.spinner1)
+        if (spinner1 != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, values)
+            spinner1.adapter = adapter
+            spinner1.setSelection(0)
+            val1 = values[0]
+
+            spinner1.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    val1 = values[position]
+                    server.val1 = val1
+                    chartUpdate()
+                    Toast.makeText(this@PlotActivity, "First signal: " + values[position],
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        // access the second spinner
+        val spinner2 = findViewById<Spinner>(R.id.spinner2)
+        if (spinner2 != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, values)
+            spinner2.adapter = adapter
+            spinner2.setSelection(1)
+            val2 = values[1]
+
+            spinner2.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    val2 = values[position]
+                    server.val2 = val2
+                    chartUpdate(is_second = true)
+                    Toast.makeText(this@PlotActivity, "Second signal: " + values[position],
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
         }
     }
 
@@ -181,5 +237,65 @@ class PlotActivity : AppCompatActivity() {
         chart.gridLabelRenderer.labelsSpace = 5
 
 
+    }
+
+    private fun chartUpdate(is_second: Boolean = false) {
+        Log.i("UPDATE", "I'm in chartUpdate()")
+        if (is_second) {
+            signal2.resetData(arrayOf<DataPoint>())
+        }
+        else {
+            signal1.resetData(arrayOf<DataPoint>())
+        }
+        var unit1 = ""
+        var unit2 = ""
+        signal1.title = val1
+        signal2.title = val2
+        when(val1){
+            "Pressure" -> {
+                chart.viewport.setMinY(260.0)
+                chart.viewport.setMaxY(1260.0)
+                unit1 = "hPa"
+            }
+            "Humidity" -> {
+                chart.viewport.setMinY(0.0)
+                chart.viewport.setMaxY(100.0)
+                unit1 = "%"
+                Log.i("VAL1", "humidity")
+            }
+            "Temperature" -> {
+                chart.viewport.setMinY(-30.0)
+                chart.viewport.setMaxY(105.0)
+                unit1 = "\u00B0" + "C"
+            }
+            else -> {
+                println("Value is unknown")
+            }
+        }
+
+        when(val2){
+            "Pressure" -> {
+                chart.secondScale.setMinY(260.0)
+                chart.secondScale.setMaxY(1260.0)
+                unit2 = "hPa"
+            }
+            "Humidity" -> {
+                chart.secondScale.setMinY(0.0)
+                chart.secondScale.setMaxY(100.0)
+                unit2 = "%"
+            }
+            "Temperature" -> {
+                chart.secondScale.setMinY(-30.0)
+                chart.secondScale.setMaxY(105.0)
+                unit2 = "\u00B0" + "C"
+                Log.i("VAL2", "temperature")
+            }
+            else -> {
+                println("Value is unknown")
+            }
+        }
+        chart.gridLabelRenderer.verticalAxisTitle = "$val1/$val2 [$unit1/$unit2]"
+//        chart.gridLabelRenderer.invalidate(true, true)
+        chart.onDataChanged(true, false)
     }
 }
